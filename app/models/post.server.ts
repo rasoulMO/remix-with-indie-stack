37c908e7aeee257f256931249cdb458886178762
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2022-06-15 23:27:47 +0200

    update prisma schema with the post data

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,16 +1,19 @@
 // this file is used to access the post on database, this is only used in the server side.
+import { prisma } from '~/db.server'
 
 export async function getPosts() {
-	const posts = [
-		{
-			slug: "hello-world",
-			title: "Hello World",
-		},
-		{
-			slug: "my-first-remix-post",
-			title: "My First Remix Post",
-		},
-	];
+	// const posts = [
+	// 	{
+	// 		slug: "hello-world",
+	// 		title: "Hello World",
+	// 	},
+	// 	{
+	// 		slug: "my-first-remix-post",
+	// 		title: "My First Remix Post",
+	// 	},
+	// ];
+	// return posts;
 
-	return posts;
+	// this will get all the posts from the database
+	return prisma.post.findMany();
 }
