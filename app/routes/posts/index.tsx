2b3c66ca52be5581236abe5f608fc61b2fc1945f
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2022-06-16 20:18:03 +0200

    added admin route

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -32,13 +32,14 @@ export default function PostsRoute() {
   return (
     <main>
       <h1>Posts</h1>
+
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link
-              to={`/posts/${post.slug}`}
-              className="text-blue-600 underline"
-            >
+            <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
             </Link>
           </li>
